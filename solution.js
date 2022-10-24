34b8190690ec86d7ab6babd2fec34bf481b7859d
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-10-24 10:56:04 -0400

    complete

--- solution.js
+++ solution.js
@@ -81,12 +81,12 @@ const sortProductNamesA = (array) => {
 };
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {
+const sortProductPriceA = (array) => {
   let unsorted = true;
   while(unsorted){
     unsorted = false;
   for(let i = 0; i < array.length-1; i++){
-    if(array[i].price < array[i+1].price){
+    if(array[i].price > array[i+1].price){
       
       [array[i],array[i+1]]=[array[i+1],array[i]]
       unsorted = true;
@@ -97,16 +97,91 @@ const sortProductPriceA = () => {
 };
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
-
+const sortProductPriceD = (array) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i].price < array[i + 1].price) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return array;
+};
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (array) => {
+  // console.log(array)
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i].price > array[i + 1].price) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        unsorted = true;
+      }
+    }
+  }
+  // console.log(array)
+
+  // let unsorted2 = true;
+  //   while(unsorted2){
+  //     unsorted2 = false;
+  //   for(let i = 0; i < array.length-1; i++){
+  //     if(array[i].name > array[i+1].name){
+  //       [array[i],array[i+1]]=[array[i+1],array[i]]
+  //       unsorted2 = true;
+  //     }
+  //   }
+  // }
+  // console.log(array)
+  return array;
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
-
+const catArtSortDesginedByA = (array) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (
+        array[i].designedBy.toLowerCase() >
+        array[i + 1].designedBy.toLowerCase()
+      ) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return array;
+};
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (arr) => {
+  const converter = (price) => {
+    if (typeof price === "number") {
+      return price;
+    } else if (price * 1 == price) {
+      return price;
+    } else {
+      return price.split("").splice(2).join() * 10;
+    }
+  };
+  let swapping = true;
+
+  while (swapping) {
+    swapping = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i].price)
+        if (converter(arr[i].price) > converter(arr[i + 1].price)) {
+          [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+          swapping = true;
+        }
+    }
+  }
+  return arr;
+};
 
 // Create your own sort function
 // it should sort in ascending order
@@ -116,8 +191,19 @@ const catArtSortByPriceA = () => {};
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {};
-
+const mySortFunction = (array) => {
+  let unsorted = true;
+  while (unsorted) {
+    unsorted = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        unsorted = true;
+      }
+    }
+  }
+  return array;
+};
 module.exports = {
   sortNumsA,
   sortNumsD,
